2b94c8bc3debe215c65f0ac76165a94cf4595231
Ali Soltani <AliSoltanipv@outlook.com>
2024-07-02 17:45:12 +0330

    REFACTOR: PROTOTYPE: sperate os manager

--- Creational/Prototype/OS-Management/Linux.cs
+++ Creational/Prototype/OS-Management/Linux.cs
@@ -39,15 +39,3 @@
     );
   }
 }
-
-public class OSManager
-{
-  private readonly Dictionary<string, IOperatingSystem> _oss = new Dictionary<string, IOperatingSystem>();
-
-  //Indexer
-  public IOperatingSystem this[string key]
-  {
-    get { return _oss[key]; }
-    set { _oss.Add(key,value); }
-  }
-}
---
+++ Creational/Prototype/OS-Management/OSManager.cs
@@ -0,0 +1,11 @@
+﻿public class OSManager
+{
+  private readonly Dictionary<string, IOperatingSystem> _oss = new Dictionary<string, IOperatingSystem>();
+
+  //Indexer
+  public IOperatingSystem this[string key]
+  {
+    get { return _oss[key]; }
+    set { _oss.Add(key,value.DeepClone()); }
+  }
+}
